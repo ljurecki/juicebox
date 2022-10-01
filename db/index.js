95a220f7f13fc7aa5c639d28904d81cbfbbea5d2
ljurecki <libette.jurecki@gmail.com>
2022-10-01 09:47:33 -0400

    Final

--- db/index.js
+++ db/index.js
@@ -2,10 +2,7 @@ const { Client } = require('pg'); // imports the pg module
 const { rows } = require('pg/lib/defaults');
 
 // supply the db name and location of the database
-const client = new Client({
-  connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
-});
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
 async function getAllUsers() {
   const { rows } = await client.query(
